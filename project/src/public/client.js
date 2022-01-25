4766978157714482f9820d19c16d666c9241becc
yechu <yechukim217@gmail.com>
2022-01-25 22:26:30 +0900

    change style

--- project/src/public/client.js
+++ project/src/public/client.js
@@ -1,5 +1,6 @@
+
 let store = {
-    user: { name: "Student" },
+    user: { name: "Friends" },
     apod: '',
     rovers: ['Curiosity', 'Opportunity', 'Spirit'],
 }
@@ -22,20 +23,17 @@ const App = (state) => {
     let { rovers, apod } = state
 
     return `
-        <header></header>
+        <div class='navbar'> 
+            <h1>Explore Mars</h1>
+            <div class='list'>
+                <a href="/">Home </a>
+                <a href="/dashboard">Dashboard</a>
+            </div>
+        </div>
+
         <main>
-            ${Greeting(store.user.name)}
+            ${Welcome(store.user.name)}
             <section>
-                <h3>Put things on the page!</h3>
-                <p>Here is an example section.</p>
-                <p>
-                    One of the most popular websites at NASA is the Astronomy Picture of the Day. In fact, this website is one of
-                    the most popular websites across all federal agencies. It has the popular appeal of a Justin Bieber video.
-                    This endpoint structures the APOD imagery and associated metadata so that it can be repurposed for other
-                    applications. In addition, if the concept_tags parameter is set to True, then keywords derived from the image
-                    explanation are returned. These keywords could be used as auto-generated hashtags for twitter or instagram feeds;
-                    but generally help with discoverability of relevant imagery.
-                </p>
                 ${ImageOfTheDay(apod)}
             </section>
         </main>
@@ -50,28 +48,21 @@ window.addEventListener('load', () => {
 
 // ------------------------------------------------------  COMPONENTS
 
-// Pure function that renders conditional information -- THIS IS JUST AN EXAMPLE, you can delete it.
-const Greeting = (name) => {
-    if (name) {
-        return `
-            <h1>Welcome, ${name}!</h1>
-        `
+const Welcome = (who) => {
+    if (who) {
+        return `<h1 class='welcome'>Welcome ${who} :) </h1>`
     }
-
-    return `
-        <h1>Hello!</h1>
-    `
+    return `<h1 class='welcome'>Welcome ;) </h1>`
 }
+
 
 // Example of a pure function that renders infomation requested from the backend
 const ImageOfTheDay = (apod) => {
+    console.log(apod)
 
     // If image does not already exist, or it is not from today -- request it again
     const today = new Date()
     const photodate = new Date(apod.date)
-    console.log(photodate.getDate(), today.getDate());
-
-    console.log(photodate.getDate() === today.getDate());
     if (!apod || apod.date === today.getDate() ) {
         getImageOfTheDay(store)
     }
@@ -97,9 +88,9 @@ const ImageOfTheDay = (apod) => {
 const getImageOfTheDay = (state) => {
     let { apod } = state
 
-    fetch(`http://localhost:3000/apod`)
+    return fetch(`http://localhost:3000/apod`)
         .then(res => res.json())
         .then(apod => updateStore(store, { apod }))
 
-    return data
+
 }
